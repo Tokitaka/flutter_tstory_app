966d4a961c9ee53cc46cd185e53aa5c1ff4dc952
Juhye O <artigen3@gmail.com>
2023-04-21 20:27:53 +0900

    user_repository

--- lib/model/user/user_repository.dart
+++ lib/model/user/user_repository.dart
@@ -4,35 +4,46 @@ import 'package:tstory_app/dto/response_dto.dart';
 import 'package:tstory_app/dto/user_request.dart';
 import 'package:tstory_app/model/user/user.dart';
 import 'package:tstory_app/provider/session_provider.dart';
+
 // 역할 : 통신 + 파싱
-class UserRepository{
+class UserRepository {
   static final UserRepository _instance = UserRepository._single();
-  factory UserRepository(){
+
+  factory UserRepository() {
     return _instance;
   }
+
   UserRepository._single();
 
-  // 로그인 성공 시 부여되는 jwt token,
-  Future<SessionUser> fetchJwtVerify() async {
-    // 1. 로그인 성공 시 생성되는 SessionUser
-    SessionUser sessionUser = SessionUser();
-    // 2. secureStorage에 담아놓은 jwt token 꺼내기 (IO)
-    String? deviceJwt = await secureStorage.read(key: "jwt");
-    if(deviceJwt != null) {
-
-    }
-  }
-
+  // // 권한 체크
+  // Future<SessionUser> fetchVerifyWithJwt() async {
+  //   // 1. 로그인 성공 시 생성되는 SessionUser
+  //   SessionUser sessionUser = SessionUser();
+  //   // 2. secureStorage에 담아놓은 jwt token 꺼내기 (IO)
+  //   String? deviceJwt = await secureStorage.read(key: "jwt");
+  //   if (deviceJwt != null) {
+  //     Response response = await dio.get("/jwtToken",
+  //         options: Options(headers: {"Authorization": deviceJwt}));
+  //     ResponseDTO responseDTO = ResponseDTO.fromJson(response.data);
+  //     responseDTO.token = deviceJwt;
+  //     responseDTO.data = User.fromJson(responseDTO.data);
+  //     if(responseDTO.code == 1) {
+  //
+  //     }
+  //   }
+  // }
   Future<ResponseDTO> fetchJoin(JoinReqDTO joinReqDTO) async {
     try {
       Response response = await dio.post("/join", data: joinReqDTO.toJson());
-      ResponseDTO responseDTO = ResponseDTO.fromJson(response.data); // data가 dynamic 타입
+      ResponseDTO responseDTO =
+          ResponseDTO.fromJson(response.data); // data가 dynamic 타입
       responseDTO.data = User.fromJson(responseDTO.data);
       return responseDTO;
     } catch (e) {
       return ResponseDTO(code: -1, msg: "유저네임 중복");
     }
   }
+
   Future<ResponseDTO> fetchLogin(LoginReqDTO loginReqDTO) async {
     // 2. 통신을 위해서 Json serialize, data에 User 객체가 json 형태로 반환되는 로직 가정
     try {
@@ -41,7 +52,7 @@ class UserRepository{
       responseDTO.data = User.fromJson(responseDTO.data);
       // 3. 토큰 받기
       final authorization = response.headers['Authorization'];
-      if(authorization !=null ){
+      if (authorization != null) {
         responseDTO.token = authorization.first;
       }
       return responseDTO; //로그인 실패 시 token 값은 null이 됨
